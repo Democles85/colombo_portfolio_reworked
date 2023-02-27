40d6b17385efe32b67f9e7f5e0d8d7bfe135c777
Sixhei Tartari <democleschannel28@gmail.com>
2023-02-27 19:56:23 +0100

    feat: Added border radius and updated styles

--- pages/api/contact.ts
+++ pages/api/contact.ts
@@ -13,13 +13,13 @@ export default async (req: NextApiRequest, res: NextApiResponse) => {
     subject: `[Message from Resonate With Yourself] ${subject}`,
     // Create HTML template for email body here
     html: `
-      <div style="width: 100%; height: 100%; background-color: #111111; color: #ffffff; padding: 20px; font-family: 'Roboto', sans-serif;">
+      <div style="width: 100%; height: 100%; background-color: #111111; color: #ffffff; padding: 20px; font-family: 'Roboto', sans-serif; border-radius: 10px;">
         <h1 style="font-size: 2rem; font-weight: 700; margin-bottom: 0;">New Message from <span style="color: #FFAF36;">Resonate with Yourself</span></h1>
-        <h2 style="font-size: 1.5rem; font-weight: 700; margin-top: 0;">${firstName} ${lastName}</h2>
-        <p style="font-size: 1.25rem; font-weight: 400; margin-bottom: 0;">${email}</p>
-        <p style="font-size: 1.25rem; font-weight: 400; margin-bottom: 0;">${phone}</p>
-        <p style="font-size: 1.25rem; font-weight: 400; margin-bottom: 0;">${country}</p>
-        <p style="font-size: 1.25rem; font-weight: 400; margin-bottom: 0;">${message}</p>
+        <h2 style="font-size: 1.5rem; font-weight: 700; margin-top: 0;"><span style="color: #FFAF36;">Name:</span> ${firstName} ${lastName}</h2>
+        <p style="font-size: 1.25rem; font-weight: 400; margin-bottom: 0;"><span style="color: #FFAF36;">Email:</span> ${email}</p>
+        <p style="font-size: 1.25rem; font-weight: 400; margin-bottom: 0;"><span style="color: #FFAF36;">Phone Number:</span> ${phone}</p>
+        <p style="font-size: 1.25rem; font-weight: 400; margin-bottom: 0;"><span style="color: #FFAF36;">Country:</span> ${country}</p>
+        <p style="font-size: 1.25rem; font-weight: 400; margin-bottom: 0;"><span style="color: #FFAF36;">Message:</span><br>${message}</p>
       </div>
     `,
   };
